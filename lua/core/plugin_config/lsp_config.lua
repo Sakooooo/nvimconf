355340bce4de15207d7f4e4ef4229f8d896725d0
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-04-21 23:49:06 +0400

    finally fixed null_ls

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,56 +1,64 @@
 require("cmp")
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "omnisharp", "pyright", "clangd"}
+	ensure_installed = { "lua_ls", "omnisharp", "pyright", "clangd" },
 })
 local mason_null_ls = require("mason-null-ls")
 
 -- vscode
 local on_attach = function(client, bufnr)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+	vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
-
 
 -- enable autocomplete
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- lint format thing
 mason_null_ls.setup({
-  ensure_installed = {
-    "stylua",
-    "csharpier",
-  },
+	ensure_installed = {
+		"stylua",
+		"csharpier",
+	},
 })
 
-
 -- funny lsp config stuff
-require("lspconfig").lua_ls.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
+require("lspconfig").lua_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-require("lspconfig").omnisharp.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
+require("lspconfig").omnisharp.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-require("lspconfig").cmake.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
+require("lspconfig").cmake.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-require("lspconfig").clangd.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
+require("lspconfig").clangd.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-require("lspconfig").pyright.setup{
-  on_attach = on_attach,
-  capabilities = capabilities
-}
+require("lspconfig").tsserver.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+require("lspconfig").pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+require("lspconfig").cssls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
--- lua/core/plugin_config/null_ls.lua
+++ lua/core/plugin_config/null_ls.lua
@@ -3,34 +3,24 @@ local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
--- wonder if this fixes it
---null_ls.setup({
---  sources = {
---    formatting.stylua,
---    formatting.csharpier,
---  }
---})
-
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-require("null-ls").setup({
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
-    sources = {
-      formatting.stylua,
-      formatting.csharpier,
-  }
+null_ls.setup({
+	sources = {
+		formatting.prettier,
+		formatting.stylua,
+		formatting.csharpier,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ buffer })
+				end,
+			})
+		end
+	end,
 })
-
